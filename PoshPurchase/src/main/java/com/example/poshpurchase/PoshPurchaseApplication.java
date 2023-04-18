16b1c5b295cc6fbd56e2d08ec52d33d87bf872dd
mimi0007 <afsanaaziz.mimi@gmail.com>
2023-04-18 17:48:42 +0600

    Initial Commit

---
+++ PoshPurchase/src/main/java/com/example/poshpurchase/PoshPurchaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.poshpurchase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoshPurchaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PoshPurchaseApplication.class, args);
+    }
+
+}
